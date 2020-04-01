360fbe5ab759bfa173343f60357d3356d6fdc4ba
ubuntu1804_smx <ubuntu1804@qq.com>
2020-03-31 23:47:14 -0700

    alternate apt source and turn off the bell of the command line

---
+++ installation/ubuntu/ubuntu1804/aptconfig.sh
@@ -0,0 +1,5 @@
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak # bakup
+sudo cp ./config/sources.list /etc/apt/sources.list
+sudo apt update 
+sudo apt upgrade
+
---
+++ installation/ubuntu/ubuntu1804/install.sh
@@ -0,0 +1,6 @@
+# alternate apt source
+sudo bash ./aptconfig.sh
+
+# remove the bell of command line
+sudo cp /etc/inputrc /etc/inputrc.bak
+sudo cp ./config/inputrc /etc/inputrc
